cb1d28593c83c04e075c16170bdd849765298ab2
pengw <wenjia.peng@siemens.com>
2019-10-18 21:54:18 -0400

    clean up test util

--- test/test-utils.js
+++ test/test-utils.js
@@ -1,86 +1,6 @@
 /* eslint-env es6 */
 
 /**
- * Converts an object to HTML string representation of attributes.
- *
- * For example: `{ foo: "bar", baz: "foo" }`
- * becomes `foo="bar" baz="foo"`
- *
- * @param {object} attributes attributes
- * @returns {string} string as HTML attributes
- */
-function _mapObjectToHTMLAttributes( attributes ) {
-    return Object.entries( attributes ).reduce( ( previous, current ) => {
-        return previous + ` ${current[0]}="${current[1]}"`;
-    }, '' );
-}
-
-
-/**
- * Replaces document's body with provided element
- * including given attributes.
- * @param {string} tag tags
- * @param {object} attributes attributes[]
- * @param {string} textContent attributes[]
- */
-function _renderToDocument( tag, attributes, textContent ) {
-    const htmlAttributes = _mapObjectToHTMLAttributes( attributes );
-    document.body.innerHTML = `<${tag} ${htmlAttributes}>${ textContent ? textContent : '' }</${tag}>`;
-}
-
-/**
- * Returns a promise which resolves as soon as
- * requested element becomes available.
- * @param {string} tag HTML tag
- * @param {boolean} waitForSubDiv if true will wait until firstchild is ready
- * @returns {Promise<HTMLElement>} promise object
- */
-async function _waitForComponentToRender( tag, waitForSubDiv ) {
-    return new Promise( resolve => {
-        /**
-         * test function
-         */
-        function requestComponent() {
-            const element = document.querySelector( tag );
-            if ( waitForSubDiv ? element && element.firstChild : element ) {
-                resolve( element );
-            } else {
-                window.requestAnimationFrame( requestComponent );
-            }
-        }
-        requestComponent();
-    } );
-}
-
-/**
- * Renders a given element with provided attributes
- * and returns a promise which resolves as soon as
- * rendered element becomes available.
- * @param {string} tag tags
- * @param {object} attributes attributes
- * @param {string} textContent attributes[]
- * @returns {Promise<HTMLElement>} Promise Object
- */
-export function render( tag, attributes = {}, textContent = '' ) {
-    _renderToDocument( tag, attributes, textContent );
-    return _waitForComponentToRender( tag );
-}
-
-/**
- * Renders a given element with provided attributes
- * and returns a promise which resolves as soon as
- * rendered element becomes available.
- * @param {string} tag tags
- * @param {object} attributes attributes
- * @param {string} textContent attributes[]
- * @returns {Promise<HTMLElement>} Promise Object
- */
-export function renderToSub( tag, attributes = {}, textContent = '' ) {
-    _renderToDocument( tag, attributes, textContent );
-    return _waitForComponentToRender( tag, true );
-}
-
-/**
  * Create tick to test async
  * @param {number} count tick count
  * @returns {Promise} promise
@@ -88,7 +8,7 @@ export function renderToSub( tag, attributes = {}, textContent = '' ) {
 export function printTick( count ) {
     let promises = [];
     for ( let i = 0; i < count; i++ ) {
-        promises.push( new Promise( ( resolve, reject ) => {
+        promises.push( new Promise( resolve => {
             setTimeout( () => {
                 // lifecycleHook.push( `tick ${i + 1}` );
                 resolve();
@@ -104,7 +24,7 @@ export function printTick( count ) {
  * @returns {Promise} promise with result
  */
 export function wait( millionseconds ) {
-    return new Promise( ( resolve, reject ) => {
+    return new Promise( resolve => {
         setTimeout( () => {
             resolve( null );
         }, millionseconds );
